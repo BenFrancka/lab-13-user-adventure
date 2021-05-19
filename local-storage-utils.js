d09b8f4e7af23ac17bccb761c3f1e9bedbf98744
Isaac <isaac.ewing3@gmail.com>
2021-05-19 16:33:12 -0600

    made the utils functions

--- local-storage-utils.js
+++ local-storage-utils.js
@@ -1,23 +1,43 @@
+import quests from './data/quests.js';
+const USER = 'USER';
+
 export function getUser() {
-    
+    const sUser = localStorage.getItem(USER);
+    if (!sUser) {
+        return ({});
+    }
+    const user = JSON.parse(sUser);
+    return (user);
 }
 
-export function setUser() {
-    
+export function setUser(user) {
+    const sUser = JSON.stringify(user);
+    localStorage.setItem(USER, sUser);
 }
 
-export function changeClout() {
-
+export function changeClout(value) {
+    const user = getUser();
+    user.clout = user.clout + value;
+    setUser(user);
 }
 
-export function cloutDeath() {
-
+export function cloutDeath(value) {
+    if (value < 1)
+        return (false);
+    return (true);
 }
 
 export function completeAll() {
-
+    const user = getUser();
+    for (let quest of quests) {
+        if (!user.completed[quest.id])
+            return (false);
+    }
+    return (true);
 }
 
-export function completeQuest() {
-
+export function completeQuest(Id) {
+    const user = getUser();
+    user.completed[Id] = true;
+    setUser(user);
 }
