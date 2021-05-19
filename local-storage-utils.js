90993681785110e1e499b18a582d6a843c95c5ce
Derek Hudgins <hudgins.derek@gmail.com>
2021-05-19 15:16:34 -0700

    added fiels' git push origin -u derek '

---
+++ local-storage-utils.js
@@ -0,0 +1,23 @@
+export function getUser() {
+    
+}
+
+export function setUser() {
+    
+}
+
+export function changeClout() {
+
+}
+
+export function cloutDeath() {
+
+}
+
+export function completeAll() {
+
+}
+
+export function completeQuest() {
+
+}
