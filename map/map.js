59c04f997e744b0aa615f5ced00f40961eb6189b
BenFrancka <benfrancka@gmail.com>
2021-05-20 12:00:41 -0700

    wrote quest js page, needs some debugging

--- map/map.js
+++ map/map.js
@@ -4,7 +4,7 @@ const questLog = document.querySelector('.quest-log');
 for (let quest of quests){
     const li = document.createElement('li');
     const a = document.createElement('a');
-    a.href = `./quest?id=${quest.id}`;
+    a.href = `../quest/?id=${quest.id}`;
     a.textContent = quest.title;
     li.append(a);
     questLog.append(li);
